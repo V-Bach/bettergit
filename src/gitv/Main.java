f705a8a416f254b0c2ad44b26b6518bb3f43666b
V-Bach <bachvuthebach@gmail.com>
2026-04-10 14:03:43 +0700

    refactoring commands with new ActionKey architecture

--- src/gitv/Main.java
+++ src/gitv/Main.java
@@ -1,10 +1,40 @@
 package gitv;
 
 import gitv.cli.CommandRouter;
+import gitv.engine.ActionKey;
+import gitv.engine.ExecutionContext;
+import gitv.engine.ExecutionEngine;
+import gitv.git.GitService;
+import gitv.workflow.Workflow;
+import gitv.workflow.WorkflowRegistry;
+import gitv.workflow.WorkflowResult;
+import gitv.workflow.basic.CommitWorkflow;
+import gitv.workflow.basic.PullWorkflow;
+import gitv.workflow.basic.PushWorkflow;
+import gitv.workflow.composite.SyncWorkflow;
 
 public class Main {
     public static void main(String[] args) {
-        CommandRouter router = new CommandRouter();
+        GitService git = new GitService();
+        WorkflowRegistry registry = new WorkflowRegistry();
+
+        registry.register(ActionKey.COMMIT, new CommitWorkflow(git));
+        registry.register(ActionKey.PUSH, new PushWorkflow(git));
+        registry.register(ActionKey.PULL, new PullWorkflow(git));
+        registry.register(ActionKey.SYNC, new SyncWorkflow(git));
+
+        registry.register(ActionKey.of("DEPLOY"), new Workflow() {
+            @Override
+            public WorkflowResult execute(ExecutionContext context) {
+                return new WorkflowResult(true, "Deployed successfully!");
+            }
+        });
+
+        registry.lock();
+
+        ExecutionEngine engine = new ExecutionEngine(registry);
+
+        CommandRouter router = new CommandRouter(engine);
         router.route(args);
     }
 }
--- src/gitv/engine/ActionType.java
+++
@@ -1,9 +0,0 @@
-package gitv.engine;
-
-public enum ActionType {
-    COMMIT,
-    PUSH,
-    PULL,
-    SYNC,
-    NONE
-}
