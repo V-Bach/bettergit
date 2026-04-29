b91c33f54563fcbab146ee1ff638fb34de1b38f7
V-Bach <bachvuthebach@gmail.com>
2026-04-29 11:01:26 +0700

    fixing for the escalation logic mode

--- src/gitv/suggestion/DecisionResult.java
+++ src/gitv/suggestion/DecisionResult.java
@@ -10,14 +10,17 @@ public class DecisionResult {
     private final Goal goal;
     private final List<ModuleIntent> intents;
     private final List<RuleResponse> appliedRules;
+    private final gitv.workflow.ExecutionMode mode;
 
-    public DecisionResult(Goal goal, List<ModuleIntent> intents, List<RuleResponse> appliedRules) {
+    public DecisionResult(Goal goal, List<ModuleIntent> intents, List<RuleResponse> appliedRules, gitv.workflow.ExecutionMode mode) {
         this.goal = goal;
         this.intents = intents;
         this.appliedRules = appliedRules;
+        this.mode = mode != null ? mode : gitv.workflow.ExecutionMode.AUTO;
     }
 
     public Goal getGoal() { return goal; }
     public List<ModuleIntent> getIntents() { return intents; }
     public List<RuleResponse> getAppliedRules() { return appliedRules; }
+    public gitv.workflow.ExecutionMode getMode() { return mode; }
 }
