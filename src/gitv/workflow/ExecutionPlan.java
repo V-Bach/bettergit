62956247e621525d8884bb36861778efe209e1b3
V-Bach <bachvuthebach@gmail.com>
2026-04-29 11:01:57 +0700

    execution routing update

--- src/gitv/workflow/ExecutionPlan.java
+++ src/gitv/workflow/ExecutionPlan.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class ExecutionPlan {
     private final List<ExecutionStep> steps;
+    private final gitv.workflow.ExecutionMode mode;
 
-    public ExecutionPlan(List<ExecutionStep> steps) {
+    public ExecutionPlan(List<ExecutionStep> steps, gitv.workflow.ExecutionMode mode) {
         this.steps = steps;
+        this.mode = mode != null ? mode : gitv.workflow.ExecutionMode.AUTO;
     }
 
     public List<ExecutionStep> getSteps() {
         return steps;
+    }
+
+    public gitv.workflow.ExecutionMode getMode() {
+        return mode;
     }
 }
